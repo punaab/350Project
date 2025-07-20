20741017c63490997fcc321668068e9076a25e17
Punaab <notbitcoinceo@gmail.com>
2025-07-19 17:11:25 -0700

    v1.0: Update application version and trigger CI/CD pipeline test

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,9 +9,9 @@ def client():
 def test_home_endpoint(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert b'Welcome to 350FinalProject!' in response.data
+    assert b'Welcome to 350FinalProject! v1.0' in response.data
 
 def test_integration_home(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert response.data.decode('utf-8') == 'Welcome to 350FinalProject!'
+    assert response.data.decode('utf-8') == 'Welcome to 350FinalProject! v1.0'
